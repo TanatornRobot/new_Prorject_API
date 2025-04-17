a89e8388f459fb8bc4d67f9e085dc145577df0df
tanatorn <tanatorn168@gmail.com>
2025-04-17 17:12:22 +0700

    เพิ่มไฟล์ HomeController.cs

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace new_Prorject_API.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("🎉 API is running. Try /api/products");
+        }
+    }
+}
